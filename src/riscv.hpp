baaf039092dfe8e6e798892c7e88a2985b9e0477
Alf-André Walla <fwsgonzo@hotmail.com>
2022-12-31 23:13:49 +0100

    Load, exec basic implementation

--- src/riscv.hpp
+++ src/riscv.hpp
@@ -16,60 +16,37 @@ class RiscvEmulator : public Control
 protected:
 	static void _bind_methods();
 
-	void _notification(int p_what);
-	bool _set(const StringName &p_name, const Variant &p_value);
-	bool _get(const StringName &p_name, Variant &r_ret) const;
-	void _get_property_list(List<PropertyInfo> *p_list) const;
-	bool _property_can_revert(const StringName &p_name) const;
-	bool _property_get_revert(const StringName &p_name, Variant &r_property) const;
-
 	String _to_string() const;
 
+public:
+	static constexpr int MARCH = 4;
+	static constexpr uint64_t MAX_INSTRUCTIONS = 16'000'000'000ULL;
+	using gaddr_t = riscv::address_type<MARCH>;
+	using machine_t = riscv::Machine<MARCH>;
+
 private:
-	Vector2 custom_position;
-	Vector3 property_from_list;
-	Vector2 dprop[3];
+	machine_t* m_machine = nullptr;
+	std::vector<uint8_t> m_binary;
 
 public:
-	// Constants.
-	enum Constants
-	{
-		FIRST,
-		ANSWER_TO_EVERYTHING = 42,
-	};
-
-	enum
-	{
-		CONSTANT_WITHOUT_ENUM = 314,
-	};
-
 	RiscvEmulator();
 	~RiscvEmulator();
 
+	auto& machine() {
+		if (m_machine != nullptr)
+			return *m_machine;
+		else
+			throw riscv::MachineException(riscv::INVALID_PROGRAM, "Machine not initialized");
+	}
+	const auto& machine() const {
+		if (m_machine != nullptr)
+			return *m_machine;
+		else
+			throw riscv::MachineException(riscv::INVALID_PROGRAM, "Machine not initialized");
+	}
+
 	// Functions.
-	void simple_func();
-	void simple_const_func() const;
-	String return_something(const String &base);
-	Viewport *return_something_const() const;
-	Variant varargs_func(const Variant **args, GDExtensionInt arg_count, GDExtensionCallError &error);
-	int varargs_func_nv(const Variant **args, GDExtensionInt arg_count, GDExtensionCallError &error);
-	void varargs_func_void(const Variant **args, GDExtensionInt arg_count, GDExtensionCallError &error);
-	void emit_custom_signal(const String &name, int value);
-	int def_args(int p_a = 100, int p_b = 200);
-
-	Array test_array() const;
-	void test_tarray_arg(const TypedArray<int64_t> &p_array);
-	TypedArray<Vector2> test_tarray() const;
-	Dictionary test_dictionary() const;
-
-	// Property.
-	void set_custom_position(const Vector2 &pos);
-	Vector2 get_custom_position() const;
-	Vector4 get_v4() const;
-
-	// Static method.
-	static int test_static(int p_a, int p_b);
-	static void test_static2();
+	void load(const PackedByteArray& buffer, const TypedArray<String>& arguments);
+	void exec();
+	void fork_exec();
 };
-
-VARIANT_ENUM_CAST(RiscvEmulator, Constants);
