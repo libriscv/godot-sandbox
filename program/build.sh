a9b81e1929f5423ceffddb28b5f360cebf2080dc
Alf-André Walla <fwsgonzo@hotmail.com>
2024-07-26 22:25:50 +0200

    Add docker file with recent RISC-V compiler

--- program/build.sh
+++ program/build.sh
@@ -1,2 +1,2 @@
 
-riscv64-linux-gnu-g++-12 -static -O2 -std=gnu++23 test.cpp variant.cpp -o test
+riscv64-linux-gnu-g++-13 -static -O2 -std=gnu++23 test.cpp variant.cpp -o test
--- program/syscalls.h
+++ program/syscalls.h
@@ -1,1 +1,22 @@
-../src/syscalls.h
+#pragma once
+#define GAME_API_BASE 500
+
+// System calls written in assembly
+#define ECALL_PRINT          (GAME_API_BASE+0)
+#define ECALL_VCALL          (GAME_API_BASE+1)
+#define ECALL_VEVAL          (GAME_API_BASE+2)
+#define ECALL_LAST           (GAME_API_BASE+3)
+
+#define STRINGIFY_HELPER(x) #x
+#define STRINGIFY(x) STRINGIFY_HELPER(x)
+
+#define MAKE_SYSCALL(number, rval, name, ...) \
+	__asm__(".pushsection .text\n" \
+		".global " #name "\n" \
+		".type " #name ", @function\n" \
+		"" #name ":\n" \
+		"	li a7, " STRINGIFY(number) "\n" \
+		"   ecall\n" \
+		"   ret\n"   \
+		".popsection .text\n"); \
+		extern "C" rval name(__VA_ARGS__);
