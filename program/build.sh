573df8fda3212f7c560ab354f0679f85b4d7d0d7
Alf-André Walla <fwsgonzo@hotmail.com>
2024-08-10 14:07:18 +0200

    Update C++ docker container to compile in parallel

--- program/build.sh
+++ program/build.sh
@@ -3,17 +3,20 @@ usage() {
 	echo "  --api        Instead of compiling, copy the API files to the output."
 	echo "   -o output   Compile sources into an output ELF file, including API and inputs."
 	echo "  --local      Compile as if locally (outside Docker), using the local API files."
+	echo "   -v          Verbose output."
 	echo "  --           Separate options from input files."
 	exit 1
 }
 
 locally=false
+verbose=false
 
 while [[ "$#" -gt 0 ]]; do
 	case $1 in
 		--api) cp -r /usr/api $1; exit ;;
 		-o) shift; output="$1"; shift; break ;;
 		--local) locally=true; shift ;;
+		-v) verbose=true; shift ;;
 		--) shift; break ;;
 		*) usage ;;
 	esac
@@ -29,10 +32,30 @@ HEAPOPS=-Wl,--wrap=malloc,--wrap=calloc,--wrap=realloc,--wrap=free
 LINKEROPS="$MEMOPS $STROPS $HEAPOPS"
 
 if [ "$locally" = true ]; then
-	# Compile locally
-	riscv64-unknown-elf-g++ -O2 -std=gnu++23 -Iapi $LINKEROPS api/*.cpp -o $output $@
+	API="api"
 else
-	LINKEROPS="-fuse-ld=mold -Wl,--execute-only $LINKEROPS"
-	# Compile in Docker
-	riscv64-linux-gnu-g++-14 -O2 -std=gnu++23 -I/usr/api $LINKEROPS /usr/api/*.cpp -o $output $@
+	API="/usr/api"
+fi
+
+# For each C++ file in *.cpp and api/*.cpp, compile it into a .o file asynchronously
+for file in $@ $API/*.cpp; do
+	if [ verbose = true ]; then
+		echo "Compiling $file"
+	fi
+	if [ "$locally" = true ]; then
+		riscv64-unknown-elf-g++ -O2 -std=gnu++23 -I$API -c $file -o $file.o &
+	else
+		riscv64-linux-gnu-g++-14 -O2 -std=gnu++23 -I$API -c $file -o $file.o &
+	fi
+done
+
+# Wait for all files to compile
+wait
+
+# Link all .o files into the output
+if [ "$locally" = true ]; then
+	riscv64-unknown-elf-g++ -static -std=gnu++23 $LINKEROPS $@.o $API/*.cpp.o -o $output
+else
+	LINKEROPS="$LINKEROPS -fuse-ld=mold -Wl,--execute-only"
+	riscv64-linux-gnu-g++-14 -static -std=gnu++23 $LINKEROPS $@.o $API/*.cpp.o -o $output
 fi
