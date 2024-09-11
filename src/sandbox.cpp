a296080e7c404d2c6c279e0770b5f5e231e6ebdd
Alf-André Walla <fwsgonzo@hotmail.com>
2024-09-11 20:24:26 +0200

    Expose set_program in Sandbox for testing and manual instantiation

--- src/sandbox.cpp
+++ src/sandbox.cpp
@@ -19,6 +19,7 @@ String Sandbox::_to_string() const {
 void Sandbox::_bind_methods() {
 	// Methods.
 	ClassDB::bind_method(D_METHOD("get_functions"), &Sandbox::get_functions);
+	ClassDB::bind_method(D_METHOD("set_program", "program"), &Sandbox::set_program);
 	{
 		MethodInfo mi;
 		mi.arguments.push_back(PropertyInfo(Variant::STRING, "function"));
