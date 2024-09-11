1e1ded9df6f613a76714d34c13c062b7ee165df1
Alf-André Walla <fwsgonzo@hotmail.com>
2024-09-11 22:52:58 +0200

    Add github action for unit tests

--- tests/run_unittests.sh
+++ tests/run_unittests.sh
@@ -1,3 +1,6 @@
 GODOT=~/Godot_v4.3-stable_linux.x86_64
 
+#$GODOT --path "$PWD" --headless --import
+#$GODOT --path "$PWD" --headless --import
+
 $GODOT --path "$PWD" --headless -s addons/gut/gut_cmdln.gd
