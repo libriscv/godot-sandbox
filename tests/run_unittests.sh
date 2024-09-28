58213be8ece5894983c59e96707c205a8bc14c16
Alf-André Walla <fwsgonzo@hotmail.com>
2024-09-28 16:27:46 +0200

    Build the unittest ELF file in GA

--- tests/run_unittests.sh
+++ tests/run_unittests.sh
@@ -1,18 +1,33 @@
 set -e
 
-GODOT=~/Godot_v4.3-stable_linux.x86_64
-
-#$GODOT --path "$PWD" --headless --import
-#$GODOT --path "$PWD" --headless --import
+# Check if unit tests are run from Github Actions
+if [ -n "$CI" ]; then
+	GODOT=./Godot_v4.3-stable_linux.x86_64
+	# Use the --import flag to properly initialize the project
+	$GODOT --path "$PWD" --headless --import
+else
+	# Use a local Godot binary
+	if [ -z "$GODOT" ]; then
+		GODOT=~/Godot_v4.3-stable_linux.x86_64
+	fi
+fi
 
 export CXX="riscv64-linux-gnu-g++-12"
 
+# Build the unit test ELF file
 mkdir -p .build
 pushd .build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=../toolchain.cmake
 make -j4
 popd
 
-cp .build/unittests tests/tests.elf
+# Create a symbolic link to the unit test ELF file
+ln -fs $PWD/.build/unittests tests/tests.elf
 
+# Import again for CI
+if [ -n "$CI" ]; then
+	$GODOT --path "$PWD" --headless --import
+fi
+
+# Run the unit tests using the GUT addon
 $GODOT --path "$PWD" --headless -s addons/gut/gut_cmdln.gd
