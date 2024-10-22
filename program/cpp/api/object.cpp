06fc9e1aee448bb59f11ba86e8905b2d6d41f779
Alf-André Walla <fwsgonzo@hotmail.com>
2024-10-22 11:03:43 +0200

    Use inline asm for new Object property get/set

--- program/cpp/api/object.cpp
+++ program/cpp/api/object.cpp
@@ -24,12 +24,40 @@ std::vector<std::string> Object::get_method_list() const {
 
 Variant Object::get(std::string_view name) const {
 	Variant var;
+#if 0
 	sys_obj_property_get(address(), name.data(), name.size(), &var);
+#else
+	register uint64_t object asm("a0") = address();
+	register const char *property asm("a1") = name.data();
+	register size_t property_size asm("a2") = name.size();
+	register Variant *var_ptr asm("a3") = &var;
+	register int syscall_number asm("a7") = ECALL_OBJ_PROP_GET;
+
+	asm volatile(
+		"ecall"
+		: "=m"(*var_ptr)
+		: "r"(object), "r"(property), "m"(*property), "r"(property_size), "r"(var_ptr), "r"(syscall_number)
+	);
+#endif
 	return var;
 }
 
 void Object::set(std::string_view name, const Variant &value) {
+#if 0
 	sys_obj_property_set(address(), name.data(), name.size(), &value);
+#else
+	register uint64_t object asm("a0") = address();
+	register const char *property asm("a1") = name.data();
+	register size_t property_size asm("a2") = name.size();
+	register const Variant *value_ptr asm("a3") = &value;
+	register int syscall_number asm("a7") = ECALL_OBJ_PROP_SET;
+
+	asm volatile(
+		"ecall"
+		:
+		: "r"(object), "r"(property), "m"(*property), "r"(property_size), "r"(value_ptr), "m"(*value_ptr), "r"(syscall_number)
+	);
+#endif
 }
 
 std::vector<std::string> Object::get_property_list() const {
