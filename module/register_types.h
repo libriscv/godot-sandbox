16872ba8f4cee3b165d1ba069a6c414f20663321
Dragos Daian <daian.dragos@yahoo.com>
2024-11-02 15:28:55 +0100

    package everything for modules

---
+++ module/register_types.h
@@ -0,0 +1,9 @@
+#ifndef GDEXTENSION_REGISTER_TYPES_H
+#define GDEXTENSION_REGISTER_TYPES_H
+
+#include "modules/register_module_types.h"
+
+void initialize_gdextension_module(ModuleInitializationLevel p_level);
+void uninitialize_gdextension_module(ModuleInitializationLevel p_level);
+
+#endif // GDEXTENSION_REGISTER_TYPES_H
