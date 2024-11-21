3102e045732d09f19a9ec4b9d1257f893d00564b
Alf-André Walla <fwsgonzo@hotmail.com>
2024-11-21 15:31:14 +0100

    Separate language into docker and project folders

--- program/rust/project/build.sh
+++ program/rust/project/build.sh
@@ -6,23 +6,21 @@ usage() {
 	echo "  --api        Instead of compiling, copy the API files to the output."
 	echo "   -o output   Compile sources into an output ELF file, including API and inputs."
 	echo "  --debug      Compile with debug information."
-	echo "  --local      Compile as if locally (outside Docker), using the local API files."
 	echo "  --version    Print the current version of the API."
 	echo "   -v          Verbose output."
 	echo "  --           Separate options from input files."
 	exit 1
 }
 
-locally=false
 verbose=false
 current_version=5
+output="rust.elf"
 
 while [[ "$#" -gt 0 ]]; do
 	case $1 in
 		--api) cp -r /usr/api $1; exit ;;
 		-o) shift; output="$1"; shift; break ;;
 		--debug) shift ;;
-		--local) locally=true; shift ;;
 		--version) shift; echo "$current_version"; exit ;;
 		-v) verbose=true; shift ;;
 		--) shift; break ;;
@@ -30,20 +28,5 @@ while [[ "$#" -gt 0 ]]; do
 	esac
 done
 
-if [ -z "$output" ]; then
-	usage
-fi
-
-# We are in /usr/src now, so what we need to do is copy the input files to the project directory
-# The project is in /usr/project
-cp $@ /usr/project/src/
-
-# Build the project
-cd /usr/project
 cargo +nightly build --release # --color never
-
-# Copy the resulting ELF file to the output
-cp target/riscv64gc-unknown-linux-gnu/release/rust_program /usr/src/$output
-
-# Remove the copied files?
-# TODO: Remove the copied files somehow
+cp target/riscv64gc-unknown-linux-gnu/release/rust_program $output
