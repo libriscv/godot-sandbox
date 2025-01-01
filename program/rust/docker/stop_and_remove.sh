01669187e291f779ae656578070b56d3f37377f7
Alf-André Walla <fwsgonzo@hotmail.com>
2025-01-01 14:19:30 +0100

    Improve Rust container scripts and enforce/suggest main.rs
    We can't build a program without a main.rs, but no hint for this except an inscrutable error. Now fixed.

--- program/rust/docker/stop_and_remove.sh
+++ program/rust/docker/stop_and_remove.sh
@@ -1,2 +1,3 @@
-docker stop -t1 godot-rust-compiler
-docker rm godot-rust-compiler
+# Ignore the error if the container is not running
+docker stop -t1 godot-rust-compiler || true
+docker rm godot-rust-compiler || true
