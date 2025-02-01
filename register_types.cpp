ee9e4023053181b6b571b11cbeeac63a5c7e3964
Dragos Daian <daian.dragos@yahoo.com>
2025-02-01 10:48:12 +0100

    update123

--- register_types.cpp
+++ register_types.cpp
@@ -2,9 +2,15 @@
 #include "core/object/object.h"
 #include "core/extension/gdextension_interface.h"
 #include "core/extension/gdextension_manager.h"
-#include "./gdextension_static_library_loader.h"
+#include "core/extension/./gdextension_static_library_loader.h"
 #include "core/object/ref_counted.h"
 #include "src/register_types.h"
+
+
+extern "C" {
+// Initialization.
+GDExtensionBool GDE_EXPORT riscv_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization);
+}
 
 void initialize_sandbox_module(ModuleInitializationLevel p_level) {
 	if (p_level != MODULE_INITIALIZATION_LEVEL_SERVERS) {
