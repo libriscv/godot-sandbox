9c710d35a543b0ffc470304110baeac99448061d
Dragos Daian <daian.dragos@yahoo.com>
2025-02-09 00:16:32 +0100

    update123

--- register_types.cpp
+++ register_types.cpp
@@ -5,6 +5,8 @@
 #include "core/extension/./gdextension_static_library_loader.h"
 #include "core/object/ref_counted.h"
 
+Ref<GDExtensionStaticLibraryLoader> loader;
+
 extern "C" {
     GDExtensionBool riscv_library_init(
         GDExtensionInterfaceGetProcAddress p_get_proc_address,
@@ -14,16 +16,20 @@ extern "C" {
 }
 
 void initialize_sandbox_module(ModuleInitializationLevel p_level) {
-	if (p_level != MODULE_INITIALIZATION_LEVEL_SERVERS) {
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
 		return;
 	}
 
-	Ref<GDExtensionStaticLibraryLoader> loader;
 	loader.instantiate();
 	loader->set_entry_funcptr((void*)&riscv_library_init);
+	ERR_PRINT("INIT SANDBOX");
 	GDExtensionManager::get_singleton()->load_extension_with_loader("sandbox", loader);
 }
 
 void uninitialize_sandbox_module(ModuleInitializationLevel p_level) {
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+		return;
+	}
 	GDExtensionManager::get_singleton()->unload_extension("sandbox");
+	loader.unref();
 }
