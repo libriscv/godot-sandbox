6990baff9121f8be5827b5238545104d4103a4b3
Dragos Daian <daian.dragos@yahoo.com>
2025-07-21 22:48:15 +0200

    fix indentaition

--- config.py
+++ config.py
@@ -2,21 +2,21 @@ def can_build(env, platform):
      return True
 
 
- def configure(env):
-     pass
+def configure(env):
+    pass
 
- def get_doc_classes():
- 	return [
-         "Sandbox",
-         "ELFScript",
-         "ELFScriptLanguage",
-         "CPPScript",
-         "CPPScriptLanguage",
-         "RustScript",
-         "RustScriptLanguage",
-         "ZigScript",
-         "ZigScriptLanguage",
- 	]
+def get_doc_classes():
+    return [
+        "Sandbox",
+        "ELFScript",
+        "ELFScriptLanguage",
+        "CPPScript",
+        "CPPScriptLanguage",
+        "RustScript",
+        "RustScriptLanguage",
+        "ZigScript",
+        "ZigScriptLanguage",
+    ]
 
- def get_doc_path():
- 	return "doc_classes"
+def get_doc_path():
+    return "doc_classes"
