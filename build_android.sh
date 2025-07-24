df9dd62f36061192ce48f042045f290316197071
Alf-André Walla <fwsgonzo@hotmail.com>
2025-07-24 10:03:37 +0200

    Update Android build script

--- build_android.sh
+++ build_android.sh
@@ -1,10 +1,18 @@
-CC=aarch64-linux-gnu-gcc-14
-CXX=aarch64-linux-gnu-g++-14
+PROJECT="/sdcard/Documents/new-game-project"
 
-mkdir -p .build_android
-pushd .build_android
-cmake .. -DCMAKE_BUILD_TYPE=Release -DRISCV_LIBTCC=ON -DANDROID_TOOLCHAIN=ON -DGODOT_DISABLE_EXCEPTIONS=OFF -DGODOTCPP_DISABLE_EXCEPTIONS=OFF -DGODOTCPP_TARGET=template_release
-make -j16
-popd
-
-mv .build_android/libgodot-riscv.so .build_android/libgodot_riscv.android.template_release.arm64.so
+## For --debug argument:
+if [ "$1" == "--debug" ]; then
+	echo "Building in debug mode..."
+	scons platform=android target=template_debug debug_symbols=yes ndk_version=24.0.8215888
+	#adb push bin/addons/godot_sandbox/bin/libgodot_riscv.android.template_release.arm64.so $PROJECT/addons/godot_sandbox/bin/libgodot_riscv.android.template_release.arm64.so
+	cp bin/addons/godot_sandbox/bin/libgodot_riscv.android.template_debug.arm64.so bin/addons/godot_sandbox/bin/libgodot_riscv.android.template_release.arm64.so
+	adb push bin/addons/godot_sandbox/bin/libgodot_riscv.android.template_debug.arm64.so $PROJECT/addons/godot_sandbox/bin/libgodot_riscv.android.template_release.arm64.so
+## For --backtrace:
+elif [ "$1" == "--backtrace" ]; then
+	adb logcat | /usr/lib/android-ndk/ndk-stack -sym bin/addons/godot_sandbox/bin
+else
+## Default case: build in release mode
+	echo "Building in release mode..."
+	scons platform=android target=template_release ndk_version=24.0.8215888
+	adb push bin/addons/godot_sandbox/bin/libgodot_riscv.android.template_release.arm64.so $PROJECT/addons/godot_sandbox/bin/libgodot_riscv.android.template_release.arm64.so
+fi
