be788de40ab91d09b16f154cb952102473516561
Alf-André Walla <fwsgonzo@hotmail.com>
2025-08-16 09:12:17 +0200

    Add scary memory-sharing test

--- tests/tests/test_shm.gd
+++ tests/tests/test_shm.gd
@@ -61,3 +61,41 @@ func test_sharing_sandboxes():
 
 	s1.queue_free()
 	s2.queue_free()
+
+func test_sharing_sandboxes_after_mutation():
+	var s1 = Sandbox.new()
+	s1.set_program(Sandbox_TestsTests)
+	s1.restrictions = true # Enable full sandboxing
+	var s2 = Sandbox.new()
+	s2.set_program(Sandbox_TestsTests)
+	s2.restrictions = true # Enable full sandboxing
+	assert_true(s1.has_function("test_shm2"), "Sandbox 1 should have the test_shm2 function")
+	assert_true(s1.has_function("verify_shm2"), "Sandbox 1 should have the verify_shm2 function")
+
+	# Create a packed array of 100k floats
+	var array = PackedFloat32Array()
+	array.resize(100000)
+
+	# Pre-share the array with both sandboxes read+write
+	var vaddr1 = s1.share_float32_array(true, array)
+	var vaddr2 = s2.share_float32_array(true, array)
+
+	# Duplicate and destroy the original array to find dragons
+	var array2 = array.duplicate()
+	array = null
+
+	await Engine.get_main_loop().process_frame
+
+	# Perform the test multiple times to ensure stability
+	for i in range(10):
+		# Call the test function in the first sandbox
+		var result = s1.vmcall("test_shm2", vaddr1, array2.size())
+		# Call the verification function in the second sandbox
+		assert_true(s2.vmcall("verify_shm2", vaddr2, array2.size()), "Verification should succeed")
+
+	# Unshare the arrays after the tests
+	assert_true(s1.unshare_array(vaddr1), "Unsharing the array should succeed")
+	assert_true(s2.unshare_array(vaddr2), "Unsharing the array should succeed")
+
+	s1.queue_free()
+	s2.queue_free()
