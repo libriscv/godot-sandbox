1553c9f988f9e174e843143a932f5ba488c9ccdc
Omega <matrac7@gmail.com>
2025-11-26 08:17:58 +0100

    Fix crash because of missing PACKED_VECTOR4_ARRAY during API generation with args. (#275)
    * Fix crash because of missing PACKED_VECTOR4_ARRAY during API generation with args.
    
    Added PACKED_VECTOR4_ARRAY in cpp_compatible_variant_type and added a test that generates API with argument names for all godot classes to detect incompatibilities in the future.
    
    * Fix crash because of missing PACKED_VECTOR4_ARRAY during API generation with args.
    
    Added PACKED_VECTOR4_ARRAY in cpp_compatible_variant_type and added a test that generates API with argument names for all godot classes to detect incompatibilities in the future.

--- src/sandbox_generated_api.cpp
+++ src/sandbox_generated_api.cpp
@@ -91,6 +91,8 @@ static const char *cpp_compatible_variant_type(int type) {
 			return "PackedArray<Vector2>";
 		case Variant::PACKED_VECTOR3_ARRAY:
 			return "PackedArray<Vector3>";
+		case Variant::PACKED_VECTOR4_ARRAY:
+			return "PackedArray<Vector4>";
 		case Variant::PACKED_COLOR_ARRAY:
 			return "PackedArray<Color>";
 		default:
